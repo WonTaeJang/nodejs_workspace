86fe9da71a0f3dfadf0190eee34c041e463f5b9c
WonTaeJang <jwt0212@gmail.com>
2019-02-20 14:18:07 +0900

    nodejs_workspace create login page _server.js create server _sample_Get.html nodejs express Get Method _sample_Post.html nodejs express Post Method

---
+++ test_01/_server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var bodyParser = require('body-parser')
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res){
+	res.send("환영합니다~");
+});
+
+app.get("/Login", function(req, res){
+
+	res.sendFile(__dirname + "/sample_Get.html");	
+});
+
+app.get("/Login_Config", function(req, res){
+	var uId = req.query.uId;
+	var uPw = req.query.uPw;
+
+	console.log("uId: " + uId + " uPw: " + uPw );
+
+	res.send("get 환영합니다~");
+	
+});
+
+app.get("/Login_Post", function(req, res){
+	
+	res.sendFile(__dirname + "/sample_Post.html");	
+});
+
+app.post("/Login_Config_Post", function(req, res){
+	var uId = req.body.uuId;
+	var uPw = req.body.uuPw;
+
+	console.log("uId: " + uId + " uPw: " + uPw );
+
+	res.send("post 환영합니다~");
+});
+
+
+http.createServer(app).listen(3000, function() {
+
+	console.log("server start get!!!");
+
+});
